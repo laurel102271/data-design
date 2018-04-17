cb4ae3d731ba6367dda6efdbec768318c6b43c11
Daniel Martinez <danny102271@sbcglobal.net>
2018-04-17 15:16:39 -0600

    Gold Gym Member Data Design

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<title>Gold Gym Member</title>
 	</head>
 	<body>
-		<p>Steve is a 27 year old male living in El Paso Texas and he is ready to join a Gold Gym. Steve has set up a appointment to sign up to the gym and is ready to start his training classes and to pay for his membership.</p>
+		<p>Steve is a 27 year old male living in El Paso Texas and he is ready to join a Gold Gym due to the fact that he has high blood pressure and is a little overweight.. Steve has set up a appointment to sign up to the gym and is ready to start his training classes and to pay for his membership.On arrival Steve meets with a personal trainer and they go over his weight goals and come up with a game plan to set a date of acheiving his goal.Steve seems to be highly motivated and he starts to discuss the membership fees and also the class training schedule.Upon discussing this further with the personal trainer Steve decides to join Gold Gym on the spot. </p>
 		<h2>Conceptual Model</h2>
 		<ul>
 			<li>memberid primary key</li>
@@ -15,13 +15,15 @@
 			<li>city</li>
 			<li>state</li>
 			<li>zip</li>
+			<li>member fee</li>
 		</ul>
 		<h2>Class</h2>
 		<ul>
 			<li>classid primary key</li>
 			<li>staffid foreign key</li>
 			<li>classDays</li>
-			<li>classTime</li>
+			<li>costofclass</li>
+			<li>classtime</li>
 		</ul>
 		<h2>Relations</h2>
 		<ul>
