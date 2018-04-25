32134f9bee36d42a0206c783c7311d9b462e34a8
Daniel Martinez <danny102271@sbcglobal.net>
2018-04-24 19:41:45 -0600

    DDL Scripts

---
+++ sql/methods.sql
@@ -0,0 +1,29 @@
+-- ins/del/update for member
+
+INSERT INTO member(memberId, memberlastName, memberFirstName, memberStreet, memberState, memberZip, );
+
+VALUES (UNHEX(REPLACE("01ceecf9-d01b-4af2-b5dcf1235891044f", "-", "")), "whatever an activation token looks like (VARCHAR(255))", "hypothetical/filepath/to/avatar.jpg", "mcpherspam@gmail.com", "1", "Marlon McPherson", "mmcph");
+
+
+
+DELETE FROM member WHERE memberId = (UNHEX(REPLACE("01ceecf9-d01b-4af2-b5dcf1235891044f", "-", ""));
+
+
+
+UPDATE member SET member =  like (VARCHAR(255))", profileAvatar = "hypothetical/filepath/to/avatar.jpg", profileEmail = "mcpherspam@gmail.com", profileIsPro = "1", profileName = "Marlon McPherson", profileUsername = "mmcph" WHERE profileId = (UNHEX(REPLACE("01ceecf9-d01b-4af2-b5dcf1235891044f", "-", ""));
+
+
+
+-- ins/del/update for training
+
+INSERT INTO training(trainingId, training Name);
+
+VALUES (UNHEX(REPLACE("1772c05b-dc60-4d18-a4b3-93f00d7f48df", "-", "")), UNHEX(REPLACE("01ceecf9-d01b-4af2-b5dcf1235891044f", "-", "")), "hypothetical/filepath/to/penCssContent", "hypothetical/filepath/to/penHtmlContent", "hypothetical/filepath/to/penJsContent", "My Pen");
+
+
+
+DELETE FROM trainig  WHERE trainingId = (UNHEX(REPLACE("1772c05b-dc60-4d18-a4b3-93f00d7f48df", "-", ""));
+
+
+
+UPDATE training SET trainingNmae = "My training",
