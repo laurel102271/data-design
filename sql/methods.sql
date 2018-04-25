7190c6d8310dfe639d6ea3d29382b1b2151badc0
Daniel Martinez <danny102271@sbcglobal.net>
2018-04-24 22:03:20 -0600

    Edit in SQL

--- sql/methods.sql
+++ sql/methods.sql
@@ -1,29 +1,42 @@
--- ins/del/update for member
+NSERT INTO member(memberId,memberLastName,memberFirstName, memberStreet, memberState, memberZip)
 
-INSERT INTO member(memberId, memberlastName, memberFirstName, memberStreet, memberState, memberZip, );
-
-VALUES (UNHEX(REPLACE("01ceecf9-d01b-4af2-b5dcf1235891044f", "-", "")), "whatever an activation token looks like (VARCHAR(255))", "hypothetical/filepath/to/avatar.jpg", "mcpherspam@gmail.com", "1", "Marlon McPherson", "mmcph");
+VALUES(UNHEX(REPLACE("f6d46fac-c4a0-49c6-bc70-fb0fbeeacb6b", "-", "")), "012345", "jullyanejgf@gmail.com", "67890", "jully");
 
 
 
-DELETE FROM member WHERE memberId = (UNHEX(REPLACE("01ceecf9-d01b-4af2-b5dcf1235891044f", "-", ""));
+UPDATE member
+
+SET memberName = "Daniel"
+
+WHERE memberName = "Daniel";
 
 
 
-UPDATE member SET member =  like (VARCHAR(255))", profileAvatar = "hypothetical/filepath/to/avatar.jpg", profileEmail = "mcpherspam@gmail.com", profileIsPro = "1", profileName = "Marlon McPherson", profileUsername = "mmcph" WHERE profileId = (UNHEX(REPLACE("01ceecf9-d01b-4af2-b5dcf1235891044f", "-", ""));
+DELETE FROM member
+
+WHERE membername = "Daniel Martinez";
 
 
 
--- ins/del/update for training
+INSERT INTO instructor(instructorId, instructorFirstName, instructorLastname, instructorPhoneNumber, )
 
-INSERT INTO training(trainingId, training Name);
-
-VALUES (UNHEX(REPLACE("1772c05b-dc60-4d18-a4b3-93f00d7f48df", "-", "")), UNHEX(REPLACE("01ceecf9-d01b-4af2-b5dcf1235891044f", "-", "")), "hypothetical/filepath/to/penCssContent", "hypothetical/filepath/to/penHtmlContent", "hypothetical/filepath/to/penJsContent", "My Pen");
+VALUES(UNHEX(REPLACE("690319b4-1111-48f1-a3c8-0003198c94db", "-", "")),
 
 
+UPDATE instructor
 
-DELETE FROM trainig  WHERE trainingId = (UNHEX(REPLACE("1772c05b-dc60-4d18-a4b3-93f00d7f48df", "-", ""));
+SET trainingInstructor = "Bob"
+
+WHERE trainingDays = "Monday";
 
 
 
-UPDATE training SET trainingNmae = "My training",
+DELETE FROM training
+
+WHERE trainingId = "f6d46fac-c4a0-49c6-bc70-fb0fbeeacb6b";
+
+
+
+SELECT * FROM instructor
+
+
